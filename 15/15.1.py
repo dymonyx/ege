6d46a14bbe0dc38ff090e840f662035be02210d2
DYMONYX <109868318+dymonyx@users.noreply.github.com>
2022-09-02 23:52:23 +0300

    Update 15.1.py

--- 15/15.1.py
+++ 15/15.1.py
@@ -2,5 +2,5 @@ def f(x, a):
     return ((x%a == 0) and (x%36!=0)) <= (x%12!=0)
 
 for a in range(1,1000):
-    if all(f(x, a) ==1  for x in range(1,10001)):
+    if all(f(x, a) ==1  for x in range(1,10001)): #x range должен быть больше range a
         print(a)
