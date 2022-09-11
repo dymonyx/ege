331e2512f4dc039af5e44c077ad9f710ba8a61cd
DYMONYX <109868318+dymonyx@users.noreply.github.com>
2022-09-11 18:42:45 +0300

    Create 12.2.py

---
+++ 12/12.2.py
@@ -0,0 +1,20 @@
+a = '01'
+while '01' in a or '02' in a or '03' in a:
+    a = a.replace('01', '302', 1)
+    a = a.replace('02', '3103', 1)
+    a = a.replace('03', '20', 1)
+print(a)
+
+a = '02'
+while '01' in a or '02' in a or '03' in a:
+    a = a.replace('01', '302', 1)
+    a = a.replace('02', '3103', 1)
+    a = a.replace('03', '20', 1)
+print(a)
+
+a = '03'
+while '01' in a or '02' in a or '03' in a:
+    a = a.replace('01', '302', 1)
+    a = a.replace('02', '3103', 1)
+    a = a.replace('03', '20', 1)
+print(a)
