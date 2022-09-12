38424af498f91e5e01cd65b1797b031674f93f2a
DYMONYX <109868318+dymonyx@users.noreply.github.com>
2022-09-12 19:35:24 +0300

    Create 16.7.py

---
+++ 16/16.7.py
@@ -0,0 +1,13 @@
+def f(n):
+    if n>30: return n*n +3*n +5
+    if n <=30 and n%2 != 0: return f(n+2) + 3*f(n+5)
+    if n <=30 and n%2 == 0: return 2*f(n+1) +f(n+4)
+
+
+k = 0
+for i in range(1,1001):
+    a = str(f(i))
+    if a.count('0') >=2:
+        k+=1
+print(k)
+    
