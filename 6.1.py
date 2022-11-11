d9cb0643d7c2bc5045282abcb660a5aea0ad96ae
DYMONYX <109868318+dymonyx@users.noreply.github.com>
2022-11-11 23:01:31 +0300

    Create 6.1.py
    Тип 6 № 47247
    
    Исполнитель Черепаха действует на плоскости с декартовой системой координат. В начальный момент Черепаха находится в начале координат, её голова направлена вдоль положительного направления оси ординат, хвост опущен. При опущенном хвосте Черепаха оставляет на поле след в виде линии. В каждый конкретный момент известно положение исполнителя и направление его движения. У исполнителя существует две команды: Вперёд n (где n — целое число), вызывающая передвижение Черепахи на n единиц в том направлении, куда указывает её голова, и Направо m (где m — целое число), вызывающая изменение направления движения на m градусов по часовой стрелке. Запись
    
    Повтори k [Команда1 Команда2 … КомандаS]
    
    означает, что последовательность из S команд повторится k раз. Черепахе был дан для исполнения следующий алгоритм:
    
    Повтори 8 [Вперёд 6 Направо 120]
    
    Определите, сколько точек с целочисленными координатами будут находиться внутри области, ограниченной линией, заданной данным алгоритмом. Точки на линии учитывать не следует.

---
+++ 6.1.py
@@ -0,0 +1,11 @@
+def f(x):
+    return 3**0.5/3 * x
+def f1(x):
+    return -3**0.5/3 * x + 6
+k = 0
+
+for x in range(1,6):
+    for y in range(1,6):
+        if y > f(x) and y < f1(x):
+            k+=1
+print(k)
