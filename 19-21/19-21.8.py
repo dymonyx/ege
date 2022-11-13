db083201f258498bf137da4601f63e8cde7dc997
DYMONYX <dymonyx13@gmail.com>
2022-11-13 19:59:00 +0300

    initial commit

---
+++ 19-21/19-21.8.py
@@ -0,0 +1,18 @@
+#2426 чет нечет
+def f(s, c, m):
+    if s >= 51: return c%2 == m%2
+    if c == m: return 0
+    h = []
+    if (s+1)%2 != 0: h+=[f(s+1, c+1, m)]
+    if (s+3)%2 != 0: h+=[f(s+3, c+1, m)]
+    if (s*3)%2 != 0: h+=[f(s*3, c+1, m)]
+    return any(h) if (c+1)%2 == m%2 else all(h)
+
+for s in range(1, 51):
+    for m in range(1, 7):
+        if f(s, 0, m) == 1:
+            print(s, m)
+            break
+#7
+#12 14
+#2
