8315a0a14f847abb56071ad6d8ff63b464456345
DYMONYX <109868318+dymonyx@users.noreply.github.com>
2022-11-13 16:45:45 +0300

    Create 19-21.5.py

---
+++ 19-21/19-21.5.py
@@ -0,0 +1,13 @@
+#чет/нечет сумма
+def f(s, c, m):
+    if s >=22: return c%2 == m%2
+    if c == m: return 0
+    h = [f(s+1, c+1, m), f(s+2, c+1, m)]
+    if s%2 != 0: h+=[f(s*2, c+1, m)]
+    return any(h) if (c+1)%2 == m%2 else all(h)
+
+for s in range(1, 22):
+    for m in range(1, 10):
+        if f(s, 0, m) == 1:
+            print(s, m)
+            break
